551bb34e47a17f89f9e0d61dffa83ad012d2e77d
simaonogueira101 <simao.nogueira66@gmail.com>
2025-03-10 08:28:35 +0100

    Adds farcaster data type.

--- app/components/FarcasterRender.tsx
+++ app/components/FarcasterRender.tsx
@@ -2,12 +2,15 @@
 
 import { useEffect, useState } from "react";
 import sdk from "@farcaster/frame-sdk";
+import type { FrameContext } from "@/app/types/farcaster";
 
 export default function Demo() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const [context, setContext] = useState<FrameContext>();
 
   useEffect(() => {
     const load = async () => {
+      setContext(await sdk.context);
       sdk.actions.ready();
     };
     if (sdk && !isSDKLoaded) {
@@ -21,9 +24,12 @@ export default function Demo() {
   }
 
   return (
-    <div className="inline-flex items-center gap-2 px-2 py-1 rounded-full bg-green-500/10 border border-green-500/20 mb-2">
-      <div className="w-2 h-2 rounded-full bg-green-500 animate-pulse"></div>
-      <span className="text-xs text-green-500">Farcaster Ready</span>
+    <div className="inline-flex items-center gap-2 px-2 py-1 rounded-full bg-green-500/10 border border-green-500/20 mb-2 justify-between">
+      <div className="flex items-center gap-2">
+        <div className="w-2 h-2 rounded-full bg-green-500 animate-pulse"></div>
+        <span className="text-xs text-green-500">Farcaster Ready</span>
+      </div>
+      <span className="text-sm">User FID: {context ? context.user.fid : "Not Loaded"}</span>
     </div>
   );
 }
---
+++ app/types/farcaster.ts
@@ -0,0 +1,22 @@
+export type FrameContext = {
+  user: {
+    fid: number;
+    username?: string;
+    displayName?: string;
+    pfpUrl?: string;
+    location?: {
+      placeId: string;
+      description: string;
+    };
+  };
+  client: {
+    clientFid: number;
+    added: boolean;
+    safeAreaInsets?: {
+      top: number;
+      left: number;
+      right: number;
+      bottom: number;
+    };
+  };
+}; 
