6e84e9111a4462980771701587d791888a35eeec
simaonogueira101 <simao.nogueira66@gmail.com>
2025-05-03 11:04:16 +0100

    Fixes hydration mismatch.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,24 +10,22 @@ export default function Navbar({ sponsored }: { sponsored?: boolean }) {
 
   return (
     <nav className="mb-3 flex items-center justify-between">
-      <Link href="/">
-        <div className="ml-1 flex items-center gap-2">
-          <Image
-            src={
-              isDarkMode
-                ? "/images/talent_protocol_icon_white.svg"
-                : "/images/base_logo_blue.svg"
-            }
-            alt="Talent Protocol"
-            fill={false}
-            height={15}
-            width={15}
-            style={{ width: "auto", height: "15px" }}
-          />
-          <h1 className="text-foreground font-semibold whitespace-nowrap">
-            Builder Rewards
-          </h1>
-        </div>
+      <Link href="/" className="ml-1 flex items-center gap-2">
+        <Image
+          src={
+            isDarkMode
+              ? "/images/talent_protocol_icon_white.svg"
+              : "/images/base_logo_blue.svg"
+          }
+          alt="Talent Protocol"
+          fill={false}
+          height={15}
+          width={15}
+          style={{ width: "auto", height: "15px" }}
+        />
+        <h1 className="text-foreground font-semibold whitespace-nowrap">
+          Builder Rewards
+        </h1>
       </Link>
 
       {sponsored && <SelectSponsor />}
