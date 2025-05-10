dcc2868ae6c57b8d24217f85357a24a33e1d74ff
simaonogueira101 <simao.nogueira66@gmail.com>
2025-05-10 09:49:27 +0100

    Data table components and functions.

--- app/components/index/ProfilesTable.tsx
+++ app/components/index/ProfilesTable.tsx
@@ -1,22 +1,85 @@
-import { columns } from "@/app/components/index/Columns";
-import { DataTable } from "@/app/components/index/DataTable";
-import { searchProfiles } from "@/app/services/index/search";
+"use client";
 
-export async function ProfilesTable() {
-  const data = await searchProfiles({
-    query: {},
-    sort: {
-      score: {
-        order: "desc",
+import { DataTable } from "@/app/components/data-table/data-table";
+import { DataTableAdvancedToolbar } from "@/app/components/data-table/data-table-advanced-toolbar";
+import { DataTableFilterList } from "@/app/components/data-table/data-table-filter-list";
+import { DataTableSortList } from "@/app/components/data-table/data-table-sort-list";
+import { useDataTable } from "@/app/hooks/useDataTable";
+import type { SearchDataResponse } from "@/app/types/index/search";
+import type { TalentProfileSearchApi } from "@/app/types/talent";
+import type { Row } from "@tanstack/react-table";
+import { useMemo } from "react";
+
+interface ProfilesTableProps {
+  initialData: SearchDataResponse;
+}
+
+export function ProfilesTable({ initialData }: ProfilesTableProps) {
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "name",
+        header: "Name",
+        enableHiding: true,
       },
+      {
+        accessorKey: "display_name",
+        header: "Display Name",
+        enableHiding: true,
+      },
+      {
+        accessorKey: "location",
+        header: "Location",
+        enableHiding: true,
+      },
+      {
+        accessorKey: "builder_score",
+        header: "Builder Score",
+        enableHiding: true,
+        cell: ({ row }: { row: Row<TalentProfileSearchApi> }) =>
+          row.original.builder_score?.points ?? "N/A",
+      },
+      {
+        accessorKey: "human_checkmark",
+        header: "Human Checkmark",
+        enableHiding: true,
+      },
+      {
+        accessorKey: "tags",
+        header: "Tags",
+        enableHiding: true,
+      },
+    ],
+    [],
+  );
+
+  const { table, shallow, debounceMs, throttleMs } = useDataTable({
+    data: initialData.profiles,
+    columns,
+    pageCount: initialData.pagination.last_page,
+    enableAdvancedFilter: true,
+    initialState: {
+      sorting: [{ id: "builder_score", desc: true }],
+      columnPinning: { right: ["actions"] },
     },
-    page: 1,
-    per_page: 25,
+    getRowId: (originalRow) => originalRow.id,
+    shallow: false,
+    clearOnDefault: true,
   });
 
   return (
-    <div>
-      <DataTable columns={columns} data={data.profiles} />
-    </div>
+    <>
+      <DataTable table={table}>
+        <DataTableAdvancedToolbar table={table}>
+          <DataTableSortList table={table} />
+          <DataTableFilterList
+            table={table}
+            shallow={shallow}
+            debounceMs={debounceMs}
+            throttleMs={throttleMs}
+          />
+        </DataTableAdvancedToolbar>
+      </DataTable>
+    </>
   );
 }
---
+++ app/lib/data-table/validations.ts
@@ -0,0 +1,33 @@
+import {
+  getFiltersStateParser,
+  getSortingStateParser,
+} from "@/app/lib/data-table/parsers";
+import { type TalentProfileSearchApi } from "@/app/types/talent";
+import {
+  createSearchParamsCache,
+  parseAsArrayOf,
+  parseAsInteger,
+  parseAsString,
+  parseAsStringEnum,
+} from "nuqs/server";
+
+export const searchParamsCache = createSearchParamsCache({
+  page: parseAsInteger.withDefault(1),
+  perPage: parseAsInteger.withDefault(10),
+  sort: getSortingStateParser<TalentProfileSearchApi>().withDefault([
+    { id: "builder_score", desc: true },
+  ]),
+  // Basic filters
+  name: parseAsString.withDefault(""),
+  display_name: parseAsString.withDefault(""),
+  location: parseAsString.withDefault(""),
+  human_checkmark: parseAsString.withDefault(""),
+  tags: parseAsArrayOf(parseAsString).withDefault([]),
+  // Advanced filter
+  filters: getFiltersStateParser().withDefault([]),
+  joinOperator: parseAsStringEnum(["and", "or"]).withDefault("and"),
+});
+
+export type GetProfilesSchema = Awaited<
+  ReturnType<typeof searchParamsCache.parse>
+>;
