aeb76ff8443e49e405eefedb02e1a2ce9e93263a
simaonogueira101 <simao.nogueira66@gmail.com>
2025-05-15 21:30:21 +0100

    Another way of accessing header.s

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -1,12 +1,40 @@
 import { headers } from "next/headers";
 
 export async function GET() {
-  const headerList = await headers();
-  const subdomain = headerList.get("x-current-subdomain");
+  const requestHeaders = await headers();
+  const headersAsObject: { [key: string]: string } = {};
+  requestHeaders.forEach((value, key) => {
+    headersAsObject[key] = value;
+  });
+  const subdomain = headersAsObject["x-current-subdomain"];
 
-  console.log("headerList from well known", headerList);
+  console.log("headerList from well known", requestHeaders);
 
   const URL = process.env.NEXT_PUBLIC_URL;
+
+  const oldJson = {
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjIwNDQyLCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4NDQ1Nzc2QzU4RDZmZkI0NWQ5YjlmNkQ2ODI0NkU5ODVFMTgzMDI2NSJ9",
+      payload: "eyJkb21haW4iOiJ3d3cuYnVpbGRlcnNjb3JlLnh5eiJ9",
+      signature:
+        "MHhiYmUwOWM4ZTljYTc1Y2RiNjUwODZkZDRmMTIwM2M4YzM2NmE1ZDlhOTNkMjYzNjU3M2VkOTRmZjU3NjAzNDY0MDk5Mjk1ODQ2MDNhZjA3MjJlNmI5YmQyNGE1YjFkMDllNWVmYTE2ZGYzYmQ4NWY3MmYxMWRjYTVhZDNhMzhmZTFi",
+    },
+    frame: {
+      version: "0.0.1",
+      name: "Builder Rewards",
+      homeUrl: "https://www.builderscore.xyz",
+      iconUrl: "https://www.builderscore.xyz/images/icon.png",
+      imageUrl: "https://www.builderscore.xyz/images/frame-image.png",
+      buttonTitle: "Earn Builder Rewards",
+      splashImageUrl: "https://www.builderscore.xyz/images/icon.png",
+      splashBackgroundColor: "#0D0740",
+      webhookUrl:
+        "https://api.neynar.com/f/app/55d7c137-31ed-4fe8-bcf4-ae48bc6b13ac/event",
+    },
+  };
+
+  console.log("oldJson", oldJson);
 
   return Response.json({
     accountAssociation: {
