70f3d7ba28a158f1d2611ecd1500590611997a7c
simaonogueira101 <simao.nogueira66@gmail.com>
2025-05-16 17:03:46 +0100

    Copy.

--- app/config/how-to-earn.ts
+++ app/config/how-to-earn.ts
@@ -55,7 +55,7 @@ export const howToEarnConfig = (
           condition: !loadingUser && selfXyzAccount,
         },
         {
-          text: "Have 1+ outgoing transaction on Celo",
+          text: "Have 1+ outgoing transaction on Celo (Coming Soon)",
           url: "https://app.talentprotocol.com",
           condition: !loadingUser && celoTransaction,
         },
