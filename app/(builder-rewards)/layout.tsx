4165ce656e2564a406eb250455184776013a868e
simaonogueira101 <simao.nogueira66@gmail.com>
2025-05-16 02:41:10 +0100

    Test removing metadata from layout.

--- app/(builder-rewards)/layout.tsx
+++ app/(builder-rewards)/layout.tsx
@@ -5,37 +5,37 @@ import UserStatus from "@/app/components/rewards/UserStatus";
 import { GrantProvider } from "@/app/context/GrantContext";
 import { LeaderboardProvider } from "@/app/context/LeaderboardContext";
 import { SponsorProvider } from "@/app/context/SponsorContext";
-import { Metadata } from "next";
-import { headers } from "next/headers";
 
-import { baseMetadata, celoMetadata } from "../lib/constants";
+// import { baseMetadata, celoMetadata } from "@/app/lib/constants";
+// import { Metadata } from "next";
+// import { headers } from "next/headers";
 
-export async function generateMetadata(): Promise<Metadata> {
-  const requestHeaders = await headers();
-  const headersAsObject: { [key: string]: string } = {};
-  requestHeaders.forEach((value, key) => {
-    headersAsObject[key] = value;
-  });
-  const subdomain = headersAsObject["x-current-subdomain"];
+// export async function generateMetadata(): Promise<Metadata> {
+//   const requestHeaders = await headers();
+//   const headersAsObject: { [key: string]: string } = {};
+//   requestHeaders.forEach((value, key) => {
+//     headersAsObject[key] = value;
+//   });
+//   const subdomain = headersAsObject["x-current-subdomain"];
 
-  let metadata;
+//   let metadata;
 
-  switch (subdomain) {
-    case "base":
-      metadata = baseMetadata;
-      break;
-    case "celo":
-      metadata = celoMetadata;
-      break;
-    case "talent-protocol":
-      metadata = baseMetadata;
-      break;
-    default:
-      metadata = baseMetadata;
-  }
+//   switch (subdomain) {
+//     case "base":
+//       metadata = baseMetadata;
+//       break;
+//     case "celo":
+//       metadata = celoMetadata;
+//       break;
+//     case "talent-protocol":
+//       metadata = baseMetadata;
+//       break;
+//     default:
+//       metadata = baseMetadata;
+//   }
 
-  return metadata;
-}
+//   return metadata;
+// }
 
 export default async function BuilderRewardsLayout({
   children,
