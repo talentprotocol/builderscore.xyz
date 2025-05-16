f88c81f7b1c9e4764f2068d0b923b53201a706b2
simaonogueira101 <simao.nogueira66@gmail.com>
2025-05-16 02:45:02 +0100

    Test adding metadata to pages.

--- app/(builder-rewards)/layout.tsx
+++ app/(builder-rewards)/layout.tsx
@@ -6,37 +6,6 @@ import { GrantProvider } from "@/app/context/GrantContext";
 import { LeaderboardProvider } from "@/app/context/LeaderboardContext";
 import { SponsorProvider } from "@/app/context/SponsorContext";
 
-// import { baseMetadata, celoMetadata } from "@/app/lib/constants";
-// import { Metadata } from "next";
-// import { headers } from "next/headers";
-
-// export async function generateMetadata(): Promise<Metadata> {
-//   const requestHeaders = await headers();
-//   const headersAsObject: { [key: string]: string } = {};
-//   requestHeaders.forEach((value, key) => {
-//     headersAsObject[key] = value;
-//   });
-//   const subdomain = headersAsObject["x-current-subdomain"];
-
-//   let metadata;
-
-//   switch (subdomain) {
-//     case "base":
-//       metadata = baseMetadata;
-//       break;
-//     case "celo":
-//       metadata = celoMetadata;
-//       break;
-//     case "talent-protocol":
-//       metadata = baseMetadata;
-//       break;
-//     default:
-//       metadata = baseMetadata;
-//   }
-
-//   return metadata;
-// }
-
 export default async function BuilderRewardsLayout({
   children,
 }: Readonly<{
--- app/(builder-rewards)/page.tsx
+++ app/(builder-rewards)/page.tsx
@@ -1,4 +1,34 @@
 import Home from "@/app/components/rewards/Home";
+import { baseMetadata, celoMetadata } from "@/app/lib/constants";
+import { Metadata } from "next";
+import { headers } from "next/headers";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const requestHeaders = await headers();
+  const headersAsObject: { [key: string]: string } = {};
+  requestHeaders.forEach((value, key) => {
+    headersAsObject[key] = value;
+  });
+  const subdomain = headersAsObject["x-current-subdomain"];
+
+  let metadata;
+
+  switch (subdomain) {
+    case "base":
+      metadata = baseMetadata;
+      break;
+    case "celo":
+      metadata = celoMetadata;
+      break;
+    case "talent-protocol":
+      metadata = baseMetadata;
+      break;
+    default:
+      metadata = baseMetadata;
+  }
+
+  return metadata;
+}
 
 export default function Page() {
   return <Home />;
