9d86c408333db283737476c1699c84340946efd2
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-05-21 14:45:57 +0300

    Handles nested queries as appropriately (except bool elements which will be next)

--- app/components/admin/QueryBuilderTester.tsx
+++ app/components/admin/QueryBuilderTester.tsx
@@ -10,6 +10,63 @@ import "react-json-view-lite/dist/index.css";
 import { QueryBuilder, RuleGroupType, formatQuery } from "react-querybuilder";
 import "react-querybuilder/dist/query-builder.css";
 
+function groupByLongestPrefix(arr) {
+  // Extract all keys
+  const keys = arr.map((obj) => Object.keys(obj[Object.keys(obj)[0]])[0]);
+
+  // Sort the keys to prepare for grouping by common prefix
+  keys.sort();
+
+  const groups = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    const currentKey = keys[i];
+    const currentParts = currentKey.split(".");
+
+    let longestPrefix = "";
+    let maxLength = 0;
+
+    for (let j = 0; j < keys.length; j++) {
+      if (i === j) continue;
+
+      const otherKey = keys[j];
+      const otherParts = otherKey.split(".");
+
+      let commonParts = [];
+      for (
+        let k = 0;
+        k < Math.min(currentParts.length, otherParts.length);
+        k++
+      ) {
+        if (currentParts[k] === otherParts[k]) {
+          commonParts.push(currentParts[k]);
+        } else {
+          break;
+        }
+      }
+
+      const commonPrefix = commonParts.join(".");
+      if (commonParts.length > 0 && commonPrefix.length > maxLength) {
+        longestPrefix = commonPrefix;
+        maxLength = commonPrefix.length;
+      }
+    }
+
+    // Fallback: use top-level prefix if no common one found
+    const prefix = longestPrefix || currentParts[0];
+    if (!groups[prefix]) {
+      groups[prefix] = [];
+    }
+
+    const obj = arr.find(
+      (o) => Object.keys(o[Object.keys(o)[0]])[0] === currentKey,
+    );
+    groups[prefix].push(obj);
+  }
+
+  return groups;
+}
+
 export default function QueryBuilderTester() {
   const [fields, setFields] = useState([]);
   const [query, setQuery] = useState<RuleGroupType>({
@@ -61,6 +118,115 @@ export default function QueryBuilderTester() {
     setDocumentSelected(e.target.value);
   };
 
+  const mergeCombinatorObjects = (
+    combinator: string,
+    query: Record<string, any>,
+  ) => {
+    console.debug("mergeCombinatorObjects: combinator", combinator);
+    for (let i = 0; i < query.length; i++) {
+      const queryItem = query[i];
+      console.debug("queryItem", queryItem);
+    }
+
+    const nonBoolItems = query.filter((item) => {
+      const operator = Object.keys(item)[0];
+      console.debug("operator", operator);
+      if (operator !== "bool") {
+        return item;
+      }
+    });
+    console.debug("nonBoolItems", nonBoolItems);
+
+    // TODO: I need to treat the bool items as well
+
+    const nestedFieldItems = nonBoolItems.filter((item) => {
+      const operator = Object.keys(item)[0];
+      const fieldName = Object.keys(item[operator])[0];
+      console.debug("fieldName", fieldName);
+      if (fieldName.includes(".")) {
+        return item;
+      }
+    });
+    console.debug("nestedFieldItems", nestedFieldItems);
+
+    const nonNestedFieldItems = nonBoolItems.filter((item) => {
+      const operator = Object.keys(item)[0];
+      const fieldName = Object.keys(item[operator])[0];
+      console.debug("fieldName", fieldName);
+      if (!fieldName.includes(".")) {
+        return item;
+      }
+    });
+    console.debug("nonNestedFieldItems", nonNestedFieldItems);
+
+    const groupedByLongestPrefix = groupByLongestPrefix(nestedFieldItems);
+    console.debug("groupedByLongestPrefix", groupedByLongestPrefix);
+    const mergedResult = {};
+    for (const group in groupedByLongestPrefix) {
+      console.debug("group name:", group);
+      const groupItems = groupedByLongestPrefix[group];
+      console.debug("group items:", groupItems);
+      mergedResult[group] = {
+        nested: {
+          path: group,
+          query: {
+            bool: {
+              [combinator]: groupItems,
+            },
+          },
+        },
+      };
+      // We need to build an object like this:
+      // {
+      //   "nested": {
+      //     "path": "scores",
+      //     "query": {
+      //       "bool": {
+      //         "must": [
+      //           { "term": { "scores.scorer": "Builder Score" } },
+      //           { "range": { "scores.points": { "gte": 0, "lte": 500 } } }
+      //         ]
+      //       }
+      //     }
+      //   }
+      // },
+    }
+    console.debug("mergedResult", mergedResult);
+
+    const mergeResultValues = Object.keys(mergedResult).map((key) => {
+      return mergedResult[key];
+    });
+
+    console.debug("mergeResultValues", mergeResultValues);
+
+    const mergeCombinatorObjectsResult =
+      nonNestedFieldItems.concat(mergeResultValues);
+    console.debug("mergeCombinatorObjectsResult", mergeCombinatorObjectsResult);
+    return mergeCombinatorObjectsResult;
+  };
+
+  const handleNestedDocuments = (formattedQuery: Record<string, any>) => {
+    console.debug("formattedQuery", formattedQuery);
+    if (formattedQuery && typeof formattedQuery === "object") {
+      for (const key in formattedQuery) {
+        console.debug("key", key);
+        if (formattedQuery.hasOwnProperty(key)) {
+          if (key === "bool") {
+            console.debug("found bool", formattedQuery[key]);
+            const combinator = Object.keys(formattedQuery[key])[0];
+            console.debug("combinator", combinator);
+            const mergeResult = mergeCombinatorObjects(
+              combinator,
+              formattedQuery[key][combinator],
+            );
+            formattedQuery[key][combinator] = mergeResult;
+          }
+        }
+      }
+    }
+    return formattedQuery;
+  };
+
   const buildQueryString = () => {
     return formatQuery(query, {
       format: "elasticsearch",
@@ -70,7 +236,7 @@ export default function QueryBuilderTester() {
   };
 
   const onClickExecuteQuery = () => {
-    const queryString = buildQueryString();
+    const queryString = handleNestedDocuments(buildQueryString());
 
     const requestBody: AdvancedSearchRequest = {
       query: {
@@ -94,7 +260,7 @@ export default function QueryBuilderTester() {
 
     (async () => {
       const response = await fetch(
-        `/api/search/advanced/${documentSelected}?${fullQueryString}`,
+        `/api/search/advanced/${documentSelected}?${fullQueryString}&debug=true`,
         {
           method: "GET",
           headers: {
@@ -140,13 +306,18 @@ export default function QueryBuilderTester() {
               fields={fields}
               query={query}
               onQueryChange={setQuery}
-              showShiftActions={true}
               controlClassnames={{ queryBuilder: "queryBuilder-branches" }}
             />
           </ClientOnly>
-          <h4>TALENT API Query</h4>
+          <h4>TALENT API Query Builder ElasticSearch Format</h4>
           <JsonView
             data={buildQueryString()}
+            shouldExpandNode={allExpanded}
+            style={defaultStyles}
+          />
+          <h4>TALENT API Query Builder After Handling Nested Documents</h4>
+          <JsonView
+            data={handleNestedDocuments(buildQueryString())}
             shouldExpandNode={allExpanded}
             style={defaultStyles}
           />
