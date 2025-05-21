dedafebc384830423ebe5f1141a135afedadcfc8
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-05-21 15:31:34 +0300

    Move the React Query Builder utility functions to lib

---
+++ app/lib/react-querybuilder-utils.ts
@@ -0,0 +1,200 @@
+import { formatQuery } from "react-querybuilder";
+
+const buildQueryString = (query) => {
+  return formatQuery(query, {
+    format: "elasticsearch",
+    parseNumbers: true,
+    preserveValueOrder: true,
+  });
+};
+
+const groupByLongestPrefix = (arr) => {
+  // Extract all keys
+  // Note that here we have objects like these:
+  // { "term": { "scores.scorer": "Builder Score" } }
+  // { "range": { "scores.points": { "gte": 0, "lte": 500 } } }
+  // So, it is not the first key of the root object, but the first key of the nested object
+  // that we need to group by.
+  const keys = arr.map((obj) => Object.keys(obj[Object.keys(obj)[0]])[0]);
+
+  // Sort the keys to prepare for grouping by common prefix
+  keys.sort();
+
+  const groups = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    const currentKey = keys[i];
+    const currentParts = currentKey.split(".");
+
+    let longestPrefix = "";
+    let maxLength = 0;
+
+    for (let j = 0; j < keys.length; j++) {
+      if (i === j) continue;
+
+      const otherKey = keys[j];
+      const otherParts = otherKey.split(".");
+
+      let commonParts = [];
+      for (
+        let k = 0;
+        k < Math.min(currentParts.length, otherParts.length);
+        k++
+      ) {
+        if (currentParts[k] === otherParts[k]) {
+          commonParts.push(currentParts[k]);
+        } else {
+          break;
+        }
+      }
+
+      const commonPrefix = commonParts.join(".");
+      if (commonParts.length > 0 && commonPrefix.length > maxLength) {
+        longestPrefix = commonPrefix;
+        maxLength = commonPrefix.length;
+      }
+    }
+
+    // Fallback: use top-level prefix if no common one found
+    const prefix = longestPrefix || currentParts[0];
+    if (!groups[prefix]) {
+      groups[prefix] = [];
+    }
+
+    const obj = arr.find(
+      (o) => Object.keys(o[Object.keys(o)[0]])[0] === currentKey,
+    );
+    groups[prefix].push(obj);
+  }
+
+  return groups;
+};
+
+const mergeCombinatorObjects = (
+  combinator: string,
+  query: Record<string, any>,
+) => {
+  console.debug("mergeCombinatorObjects: combinator", combinator);
+  for (let i = 0; i < query.length; i++) {
+    const queryItem = query[i];
+    console.debug("queryItem", queryItem);
+  }
+
+  const nonBoolItems = query.filter((item) => {
+    const operator = Object.keys(item)[0];
+    console.debug("operator", operator);
+    if (operator !== "bool") {
+      return item;
+    }
+  });
+  console.debug("nonBoolItems", nonBoolItems);
+
+  // TODO: I need to treat the bool items as well
+  const boolItems = query.filter((item) => {
+    const operator = Object.keys(item)[0];
+    console.debug("operator", operator);
+    if (operator === "bool") {
+      return item;
+    }
+  });
+  console.debug("boolItems", boolItems);
+  const boolItemsHandled = boolItems.map((boolItem) => {
+    return handleNestedDocuments(boolItem);
+  });
+  console.debug("boolItems after handleNestedDocuments", boolItemsHandled);
+
+  const nestedFieldItems = nonBoolItems.filter((item) => {
+    const operator = Object.keys(item)[0];
+    const fieldName = Object.keys(item[operator])[0];
+    console.debug("fieldName", fieldName);
+    if (fieldName.includes(".")) {
+      return item;
+    }
+  });
+  console.debug("nestedFieldItems", nestedFieldItems);
+
+  const nonNestedFieldItems = nonBoolItems.filter((item) => {
+    const operator = Object.keys(item)[0];
+    const fieldName = Object.keys(item[operator])[0];
+    console.debug("fieldName", fieldName);
+    if (!fieldName.includes(".")) {
+      return item;
+    }
+  });
+  console.debug("nonNestedFieldItems", nonNestedFieldItems);
+
+  const groupedByLongestPrefix = groupByLongestPrefix(nestedFieldItems);
+  console.debug("groupedByLongestPrefix", groupedByLongestPrefix);
+  const mergedResult = {};
+  for (const group in groupedByLongestPrefix) {
+    console.debug("group name:", group);
+    const groupItems = groupedByLongestPrefix[group];
+    console.debug("group items:", groupItems);
+    mergedResult[group] = {
+      nested: {
+        path: group,
+        query: {
+          bool: {
+            [combinator]: groupItems,
+          },
+        },
+      },
+    };
+    // We need to build an object like this:
+    // {
+    //   "nested": {
+    //     "path": "scores",
+    //     "query": {
+    //       "bool": {
+    //         "must": [
+    //           { "term": { "scores.scorer": "Builder Score" } },
+    //           { "range": { "scores.points": { "gte": 0, "lte": 500 } } }
+    //         ]
+    //       }
+    //     }
+    //   }
+    // },
+  }
+  console.debug("mergedResult", mergedResult);
+
+  const mergeResultValues = Object.keys(mergedResult).map((key) => {
+    return mergedResult[key];
+  });
+
+  console.debug("mergeResultValues", mergeResultValues);
+
+  const mergeCombinatorObjectsResult = nonNestedFieldItems
+    .concat(mergeResultValues)
+    .concat(boolItemsHandled);
+  console.debug("mergeCombinatorObjectsResult", mergeCombinatorObjectsResult);
+  return mergeCombinatorObjectsResult;
+};
+
+const handleNestedDocuments = (formattedQuery: Record<string, any>) => {
+  console.debug("formattedQuery", formattedQuery);
+  if (formattedQuery && typeof formattedQuery === "object") {
+    for (const key in formattedQuery) {
+      console.debug("key", key);
+      if (formattedQuery.hasOwnProperty(key)) {
+        if (key === "bool") {
+          console.debug("found bool", formattedQuery[key]);
+          const combinator = Object.keys(formattedQuery[key])[0];
+          console.debug("combinator", combinator);
+          const mergeResult = mergeCombinatorObjects(
+            combinator,
+            formattedQuery[key][combinator],
+          );
+          formattedQuery[key][combinator] = mergeResult;
+        }
+      }
+    }
+  }
+  return formattedQuery;
+};
+
+export {
+  buildQueryString,
+  groupByLongestPrefix,
+  handleNestedDocuments,
+  mergeCombinatorObjects,
+};
