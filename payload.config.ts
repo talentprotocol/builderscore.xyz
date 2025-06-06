71ff67de026c1f620d682000247d94e987ce07d4
simaonogueira101 <simao.nogueira66@gmail.com>
2025-06-06 19:46:57 +0100

    Vercel blob.

--- payload.config.ts
+++ payload.config.ts
@@ -8,6 +8,7 @@ import { Users } from "@/collections/Users";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import { payloadCloudPlugin } from "@payloadcms/payload-cloud";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { vercelBlobStorage } from "@payloadcms/storage-vercel-blob";
 import path from "path";
 import { buildConfig } from "payload";
 import sharp from "sharp";
@@ -49,6 +50,12 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
 });
