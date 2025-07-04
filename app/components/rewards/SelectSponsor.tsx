90e3b6358db077f09791e312f3e228849a4894bf
simaonogueira101 <simao.nogueira66@gmail.com>
2025-07-04 16:04:19 +0200

    Fix select.

--- app/components/rewards/SelectSponsor.tsx
+++ app/components/rewards/SelectSponsor.tsx
@@ -8,7 +8,6 @@ import {
   SelectValue,
 } from "@/app/components/ui/select";
 import { useSponsor } from "@/app/context/SponsorContext";
-import { useHistoryListener } from "@/app/hooks/useHistoryListener";
 import { useSponsors } from "@/app/hooks/useRewards";
 import { ALLOWED_SPONSORS } from "@/app/lib/constants";
 import { Sponsor } from "@/app/types/rewards/sponsors";
@@ -29,16 +28,6 @@ export default function SelectSponsor() {
     pathSegments[1] = newSponsor;
     window.history.pushState(null, "", pathSegments.join("/"));
   };
-
-  useHistoryListener((url) => {
-    const pathname = url.startsWith("http") ? new URL(url).pathname : url;
-    const pathSegments = pathname.split("/");
-    const sponsorSlug = pathSegments[1] || "";
-
-    if (sponsorSlug && sponsorSlug !== selectedSponsor?.slug) {
-      setSelectedSponsorFromSlug(sponsorSlug);
-    }
-  });
 
   return (
     <Select value={selectedSponsor?.slug} onValueChange={handleSponsorChange}>
